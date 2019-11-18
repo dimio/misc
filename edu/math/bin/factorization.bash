81fe44fed890a02a4627243c814728e563f8360d
Dimio <dimio@dimio.org>
2019-11-18 15:44:55 +0300

    added factorization

---
+++ edu/math/bin/factorization.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# v. 0.0.2
+# dimio (dimio.org)
+set -e
+set -u
+
+MIN=$1
+MAX=$2
+SUM=$(shuf -i ${MIN}-${MAX} -n 1)
+if [[ ${SUM} != ${MIN} ]]; then
+	ADDEND1=$(shuf -i 1-$(expr ${SUM} - 1) -n 1)
+else
+	ADDEND1=0
+fi
+ADDEND2=$(expr ${SUM} - ${ADDEND1})
+ADDEND2LNGHT=${#ADDEND2}
+
+printf "  %2d \n" ${SUM}
+printf "  / \ \n"
+printf " %d + _" ${ADDEND1}
+read -s -N ${ADDEND2LNGHT} x
+printf "\b%d" ${x}
+while [[ ${x} != ${ADDEND2} ]]
+do
+	read -s -N ${ADDEND2LNGHT} x
+	printf "\b%d" ${x}
+done
---
+++ edu/math/run.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# v. 0.0.2
+# dimio (dimio.org)
+set -e
+set -u
+
+BIN="$(dirname $0)/bin"
+FACT="${BIN}/factorization.bash"
+
+_cyclical_call(){
+	for ((i=1; i <= ${1}; i++))
+	do
+		printf "\n\nПример # ${i} из ${1}\n"
+		# bash -i ${EXEC} ${2} ${3}
+		eval ${2} ${3} ${4}
+	done
+}
+
+_show_help() {
+cat <<EOF
+$0 [-fh] repeats min max
+	Options:
+		-f | --factorization	run <repeats> additions example in <min> and <max> diapason
+		-h | --help				show this help and exit
+		repeats					count of repetition of tasks
+		min, max				bottom and top limits for generated number
+
+	Example:
+		$0 -f 2 1 10
+		generate 2 tasks with the sum for the decomposition on the terms from 1 to 10:
+		6 = 3 + X
+		X= ?
+EOF
+}
+
+if [[ $# != 0 ]]; then
+    case "${1}" in
+        -f|--factorization)
+			_cyclical_call "${2}" "${FACT}" "${3}" "${4}";;
+		-h|--help)
+			_show_help;;
+    esac
+else
+    _show_help
+fi
